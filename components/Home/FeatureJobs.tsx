e11ae914bdeda9fd0c18643762123da009d2792c
ElVatoEste <bigmalv@gmail.com>
2025-05-27 14:24:43 -0600

    Subida de vercel.

--- components/Home/FeatureJobs.tsx
+++ components/Home/FeatureJobs.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import { Heading, JobCard } from '../../paths'
-import JobData from '@/data'
+import { Heading} from '../../paths'
 import Link from 'next/link'
 const FeatureJobs = () => {
     return (
         <div>
             <Heading mainHeading={'Trabajos destacados'} subHeading={'Conoce tu valor y encuentra el trabajo que mejore tu calidad de vida'} />
 
-            <div className='mt-12 w-[80%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-12'>
-                {JobData?.map((job) => {
-                    return <Link key={job?.id} href={`job/jobDetails/${job?.id}`}>
-                        <JobCard job={job} />
-                    </Link>
-                })}
-
-            </div>
 
             <Link href={"/job/alljobs"}>
                 <div className='text-center mt-12'>
