abf452c8cfeb243fc568ac7b671db147641f68d9
ElVatoEste <bigmalv@gmail.com>
2025-05-29 02:12:54 -0600

    Actualizacion para vercel

--- utils/firebaseAdmin.ts
+++ utils/firebaseAdmin.ts
@@ -1,11 +1,17 @@
 // utils/firebaseAdmin.ts
-import { getApps, initializeApp, cert } from 'firebase-admin/app';
+import { cert, getApps, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import * as serviceAccount from '../firebase-service-account.json'; // <--- ubicá el JSON aquí
+import type { ServiceAccount } from 'firebase-admin';
+
+const serviceAccount: ServiceAccount = {
+  projectId:   process.env.FIREBASE_PROJECT_ID!,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+};
 
 if (!getApps().length) {
   initializeApp({
-    credential: cert(serviceAccount as any),
+    credential: cert(serviceAccount),
   });
 }
 
